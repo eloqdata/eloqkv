c97462064af7615480229c1e0d22ccd7add123ac
JoJo <zx910819zx@163.com>
2025-08-15 19:15:20 +0800

    update

--- concourse/scripts/build_tarball.bash
+++ concourse/scripts/build_tarball.bash
@@ -68,7 +68,7 @@ arm64 | aarch64) ARCH=arm64 ;;
 *) ARCH= $(uname -m) ;;
 esac
 
-if [ -n "${TAGGED}" ]; then
+if [ "${TAGGED}" = "true" ]; then
     TAGGED=$(git tag --sort=-v:refname | head -n 1)
     if [ -z "${TAGGED}" ]; then
         exit 1
@@ -184,7 +184,7 @@ patchelf --set-rpath '$ORIGIN' ${DEST_DIR}/lib/librocksdb*
 cp ${ELOQKV_SRC}/concourse/artifact/eloqkv.ini ${DEST_DIR}/conf/
 tar -czvf eloqkv.tar.gz -C ${HOME} EloqKV
 
-if [ -n "${TAGGED}" ]; then
+if [ "${TAGGED}" = "true" ]; then
     TX_TARBALL="eloqkv-${TAGGED}-${OS_ID}-${ARCH}.tar.gz"
     eval ${INSTALL_PSQL}
     SQL="INSERT INTO tx_release VALUES ('eloqkv', '${ARCH}', '${OS_ID}', '${DATA_STORE_ID}', $(echo ${TAGGED} | tr '.' ',')) ON CONFLICT DO NOTHING"
@@ -237,7 +237,7 @@ build_upload_log_srv() {
 
 if [ "${BUILD_LOG_SRV}" = true ]; then
     # make and build log_service
-    if [ -n "${TAGGED}" ]; then
+    if [ "${TAGGED}" = "true" ]; then
         LOG_TARBALL="log-service-${TAGGED}-${OS_ID}-${ARCH}.tar.gz"
     else
         LOG_TARBALL="log-service-${OUT_NAME}-${OS_ID}-${ARCH}.tar.gz"
