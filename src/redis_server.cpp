c58e89bbe3b0cc898a75d7459ca27e946cae3a4d
concourse-ci <concourse@noreply.com>
2025-09-09 03:23:52 +0000

    New tag 0.8.20

--- src/redis_server.cpp
+++ src/redis_server.cpp
@@ -34,7 +34,7 @@
 #include "redis_service.h"
 
 DEFINE_string(config, "", "Configuration");
-constexpr char VERSION[] = "0.8.19";
+constexpr char VERSION[] = "0.8.20";
 
 // Global variable defined in redis_service.cpp
 extern brpc::Acceptor *EloqKV::server_acceptor;
