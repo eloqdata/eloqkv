6407acb001a3faf28dc9d92ab686bfb25219333b
githubzilla <githubzilla@gmail.com>
2025-09-26 12:03:44 +0800

    ROCKSDB_CLOUD_FS_TYPE is not used by DSS

--- include/redis_service.h
+++ include/redis_service.h
@@ -51,7 +51,7 @@
 #endif
 
 #if defined(DATA_STORE_TYPE_DYNAMODB) ||                                       \
-    (ROCKSDB_CLOUD_FS_TYPE == ROCKSDB_CLOUD_FS_TYPE_S3)
+    defined(DATA_STORE_TYPE_ELOQDSS_ROCKSDB_CLOUD_S3)
 #include <aws/core/Aws.h>
 #endif
 
@@ -536,7 +536,7 @@ private:
 
     std::unique_ptr<TxService> tx_service_;
 #if defined(DATA_STORE_TYPE_DYNAMODB) ||                                       \
-    (ROCKSDB_CLOUD_FS_TYPE == ROCKSDB_CLOUD_FS_TYPE_S3)
+    defined(DATA_STORE_TYPE_ELOQDSS_ROCKSDB_CLOUD_S3)
     Aws::SDKOptions aws_options_;
 #endif
 
