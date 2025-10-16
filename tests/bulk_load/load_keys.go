06095dc99fc99221e47cdbf5cb552f51c5abaec5
Chen Zhao <ch3nzhao@gmail.com>
2025-10-16 18:36:56 +0800

    load random keys

--- tests/bulk_load/load_keys.go
+++ tests/bulk_load/load_keys.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"crypto/rand"
 	"flag"
 	"fmt"
 	"log"
+	"math/big"
 	"runtime"
 	"strconv"
 	"sync"
@@ -22,6 +24,7 @@ var (
 	startKey  = flag.Int("start_key", 1, "Starting key index")
 	threads   = flag.Int("threads", 1, "Number of worker threads (GOMAXPROCS)")
 	conns     = flag.Int("conns", 1, "Total number of parallel connections/jobs")
+	randomVal = flag.Bool("random", false, "Generate random value data for each request")
 )
 
 var uploaded int64
@@ -34,11 +37,6 @@ func main() {
 	runtime.GOMAXPROCS(*threads)
 
 	fmt.Printf("Launching %d connections for keys %d ~ %d (stride mode)\n", *conns, *startKey, *totalKeys)
-
-	value := make([]byte, *dataSize)
-	for i := range value {
-		value[i] = 'x'
-	}
 
 	done := make(chan struct{})
 	go func() {
@@ -62,7 +60,7 @@ func main() {
 		wg.Add(1)
 		go func(id int) {
 			defer wg.Done()
-			doJob(id, value)
+			doJob(id)
 		}(i)
 	}
 	wg.Wait()
@@ -71,15 +69,26 @@ func main() {
 	fmt.Println("All jobs finished")
 }
 
-func doJob(id int, value []byte) {
+func doJob(id int) {
 	ctx := context.Background()
 	addr := fmt.Sprintf("%s:%d", *server, *port)
 	rdb := redis.NewClient(&redis.Options{Addr: addr})
+
+	value := make([]byte, *dataSize)
+	if !*randomVal {
+		for i := range value {
+			value[i] = 'x'
+		}
+	}
 
 	fmt.Printf("Job %d: inserting keys kv_%d, kv_%d, ... (stride %d)\n",
 		id, *startKey+id, *startKey+id+*conns, *conns)
 
 	for k := *startKey + id; k <= *totalKeys; k += *conns {
+		if *randomVal {
+			generateRandom(value)
+		}
+
 		key := "kv_" + strconv.Itoa(k)
 		for {
 			err := rdb.Set(ctx, key, value, 0).Err()
@@ -91,5 +100,13 @@ func doJob(id int, value []byte) {
 			atomic.AddInt64(&uploaded, 1)
 			break
 		}
+	}
+}
+
+func generateRandom(buf []byte) {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	for i := range buf {
+		nBig, _ := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		buf[i] = letters[nBig.Int64()]
 	}
 }
