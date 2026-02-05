d592989e6071987436b22ff4e5512470173cd592
js0.site <jssite@googlegroups.com>
2026-02-05 15:31:59 +0800

    Refactor: Move build-args to inputs and utilize organization vars
    - Remove build-args for secrets in docker.template.yml
    - Pass BUILDER_IMAGE via inputs using vars.DOCKER_ORG instead of secrets
    - Restore 'secrets: inherit' for cleaner workflow configuration

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
-DIR=$(realpath $0) && DIR=${DIR%/*/*}
-cd $DIR
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR/..
 set -x
 
 git submodule update --init --recursive
-docker build .
+docker build -f $DIR/docker/eloqkv.docker .
