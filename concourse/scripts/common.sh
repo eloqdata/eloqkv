1417b3f27015cd20104711d9a14d23af1d6ad982
githubzilla <githubzilla@gmail.com>
2026-03-15 12:44:23 +0800

    Move single test to piror

--- concourse/scripts/common.sh
+++ concourse/scripts/common.sh
@@ -2313,6 +2313,9 @@ function run_eloq_test() {
     sed -i "s/eloq_store_cloud_secret_key.*=.\+/eloq_store_cloud_secret_key=${rocksdb_cloud_aws_secret_access_key}/g" ./bootstrap_cnf/*_eloqdss_eloqstore.cnf
     sed -i "s/eloq_store_cloud_store_path.*=.\+/eloq_store_cloud_store_path=${eloqstore_cloud_store_path}/g" ./bootstrap_cnf/*_eloqdss_eloqstore.cnf
 
+    rm -rf runtime/*
+    python3 run_tests.py --dbtype redis --group single --storage eloqdss-eloqstore --install_path ${eloqkv_install_path}
+
     # run single/multi test
     rm -rf runtime/*
     python3 redis_test/multi_test/smoke_test.py --dbtype redis --storage eloqdss-eloqstore --install_path ${eloqkv_install_path} --bootstrap true
@@ -2328,8 +2331,6 @@ function run_eloq_test() {
     rm -rf runtime/*
     python3 run_tests.py --dbtype redis --group standby --storage eloqdss-eloqstore --install_path ${eloqkv_install_path} --bootstrap true
 
-    rm -rf runtime/*
-    python3 run_tests.py --dbtype redis --group single --storage eloqdss-eloqstore --install_path ${eloqkv_install_path}
     # rm -rf runtime/*
     # python3 redis_test/standby_test/test_with_kv.py --dbtype redis --storage eloqdss-eloqstore --install_path ${eloqkv_install_path}
     # sleep 1
