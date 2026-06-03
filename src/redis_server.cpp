d3348212ca5d6c62d66b9c698b6bd03cb8773547
starrysky <starrysky9651@outlook.com>
2026-06-03 15:52:55 +0800

    chore: bump version to 1.2.3

--- src/redis_server.cpp
+++ src/redis_server.cpp
@@ -38,7 +38,7 @@
 #include "redis_service.h"
 
 DEFINE_string(config, "", "Configuration");
-constexpr char VERSION[] = "1.2.2";
+constexpr char VERSION[] = "1.2.3";
 
 // EloqKV flags - these are converted to tx flags
 DEFINE_string(ip, "127.0.0.1", "Redis IP");
